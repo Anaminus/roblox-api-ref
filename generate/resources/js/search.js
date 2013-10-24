b90b7bfb28c6b79bab9ee7d41135fc5e4eeb5e9a
Anaminus <anaminus@no-email>
2013-10-24 08:55:36 -0500

    commentary

--- generate/resources/js/search.js
+++ generate/resources/js/search.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
+	// Users with javascript disabled wont be able to use the search, so
+	// inject the elements.
 	$('#header').append('<span id="search-form"><label>Search: </label></span>')
 	var searchInput = $('<input id="search-input" type="text" placeholder="Class or Member"></input>').appendTo('#search-form > label');
+
 	var boxContent = $("#box-content");
 
 	var fuse;
@@ -26,13 +29,17 @@ $(document).ready(function() {
 		firstResult = null;
 
 		if (text.length == 0) {
+			// input field is empty; display normal page content
 			searchResults.css('display', 'none')
 			boxContent.css('display', 'block')
 		} else {
+			// user has provided search input; display search results
 			searchResults.css('display', 'block')
 			boxContent.css('display', 'none')
 
+			// Do a fuzzy search on the search database
 			var results = fuse.search(text);
+			// Limit number of results
 			var max = results.length > 50 ? 50 : results.length;
 			for (i = 0; i < max; i++) {
 				var result = results[i]
@@ -47,14 +54,20 @@ $(document).ready(function() {
 		}
 	}
 
+	// Instant-like search; show results as the user types.
 	var timer;
 	searchInput.on("input", function() {
 		timer && clearTimeout(timer);
 		timer = window.setTimeout(doSearch, 200);
 	})
+
+	// Take the user to the first result if they press enter.
 	searchInput.keydown(function(event) {
 		if (event.which == 13 && firstResult) {
 			window.location.href = firstResult;
 		}
 	})
+
+	// TODO: Do search if input value is not empty, in case the user gave
+	// input before we've finished initializing
 })
