e5d7314a3519176bf0682cecc8970d844574b8b6
Anaminus <anaminus@no-email>
2013-10-24 09:09:54 -0500

    more improvements

--- generate/resources/js/search.js
+++ generate/resources/js/search.js
@@ -1,22 +1,30 @@
 $(document).ready(function() {
-	// Users with javascript disabled wont be able to use the search, so
-	// inject the elements.
+	// Users with javascript disabled wont be able to use the search, so we'll
+	// inject the search form so that it isn't visible to them.
 	$('#header').append('<span id="search-form"><label>Search: </label></span>')
 	var searchInput = $('<input id="search-input" type="text" placeholder="Class or Member"></input>').appendTo('#search-form > label');
 
 	var boxContent = $("#box-content");
 
 	var fuse;
+	// Retrieve search database, which contains class and member names to
+	// search for. If the user has javascript disabled, then this doesn't get
+	// loaded, saving a few tubes.
 	$.getJSON('/api/search-db.json', function(data) {
 		fuse = new Fuse(data, {
+			// c: Class name; m: Member name
 			keys: ['c', 'm'],
+			// 0: Needs perfect match; 1: Matches anything
+			// With testing, 0.3 seemed decent enough
 			threshold: 0.3
 		})
 	})
 
+	// Inject search results
 	var searchResults = $('<div id="search-results"></div>').appendTo('#ref-box');
 	var resultContainer = $('<ul></ul>').appendTo(searchResults);
 
+	// Holds the URL of the first result, in case the user presses enter
 	var firstResult;
 
 	function doSearch() {
@@ -29,22 +37,26 @@ $(document).ready(function() {
 		firstResult = null;
 
 		if (text.length == 0) {
-			// input field is empty; display normal page content
+			// Input field is empty; display normal page content
 			searchResults.css('display', 'none')
 			boxContent.css('display', 'block')
 		} else {
-			// user has provided search input; display search results
-			searchResults.css('display', 'block')
+			// User has provided search input; display search results
 			boxContent.css('display', 'none')
+			searchResults.css('display', 'block')
 
 			// Do a fuzzy search on the search database
 			var results = fuse.search(text);
+
 			// Limit number of results
 			var max = results.length > 50 ? 50 : results.length;
+
 			for (i = 0; i < max; i++) {
 				var result = results[i]
 				var member = result.m
+				// URL can be derived from the class and member name
 				var url = '/api/class/' + result.c + '.html' + (member ? '#member' + member : '')
+				// Database also includes an icon index we can use for prettier results
 				var icon = member ? 'api-member-icon' : 'api-class-icon'
 				resultContainer.append('<li><a href="' + url + '"><span class="' + icon + '" style="background-position:' + result.i * -16 + 'px center"></span>' + result.c + (member ? '.' + member : '') + '</a></li>');
 				if (i == 0) {
@@ -68,6 +80,9 @@ $(document).ready(function() {
 		}
 	})
 
-	// TODO: Do search if input value is not empty, in case the user gave
-	// input before we've finished initializing
+	// In case the user manages to input a value before we've finished
+	// initializing.
+	if (searchInput.val().length > 0) {
+		doSearch();
+	}
 })
